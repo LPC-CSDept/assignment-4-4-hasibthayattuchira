837ac461b24f5780b33243c0e2498b5d8b5e526a
hasibthayattuchira <hthayattuchira@gmail.com>
2024-07-02 11:19:18 -0400

    Create main.py

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 def main():
 
     numbers = []
+    for i in range(5):
+        num = int(input("Input a number: "))
+        numbers.append(num)
+    
+    for num in numbers:
+        if num < minval:
+            minval = num
+        elif num > maxval:
+            maxval = num
     """
     ########################################
     Code Your Program here
